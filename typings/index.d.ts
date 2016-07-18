c10637c658dffeb21f0d76c3ae389eb7cb7e2bbb
Yegor Sytnyk <yegor.sytnyk@gmail.com>
2016-07-18 10:46:23 +1000

    clean server build folder

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -31,3 +31,4 @@
 /// <reference path="modules/react-redux/index.d.ts" />
 /// <reference path="modules/redux-thunk/index.d.ts" />
 /// <reference path="modules/redux/index.d.ts" />
+/// <reference path="modules/rimraf/index.d.ts" />
